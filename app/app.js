fb11607d33e8b875fa999ec92c0b45f90dcb7d4e
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-07 17:28:43 -0500

    feature: router method

--- app/app.js
+++ app/app.js
@@ -1,42 +1,32 @@
-//why is Prettier adding a comma after aboutModel but not aboutView??
 import { homeModel, blogModel, aboutModel } from "./models/model.js";
 
 import { homeView, blogView, aboutView, contactView } from "./views/view.js";
 
-const homeContent = homeView(homeModel);
-const blogContent = blogView(blogModel);
-const aboutContent = aboutView(aboutModel);
-const contactContent = contactView;
-
 const titleBase = "TV Blog";
 
-const changeContent = (contentID) => $("#app").html(eval(contentID));
+const routes = {
+  home: homeView(homeModel),
+  blog: blogView(blogModel),
+  about: aboutView(aboutModel),
+  contact: contactView,
+};
 
 const changeRoute = () => {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
-  console.log(hashTag + " " + pageID);
-  let contentID = pageID + "Content";
-  changeContent(contentID);
+
+  //set to home if empty string
+  if (pageID == "") {
+    pageID = "home";
+  }
+  document.getElementById("app").innerHTML = routes[pageID];
   $(document).attr("title", `${titleBase} | ${pageID.toUpperCase()}`);
 };
 
-const setHome = () => {
-  $("#app").html(homeContent);
-};
-
 function initListeners() {
-  // $("nav a").click(function (e) {
-  //   let aID = e.currentTarget.id;
-  //   $(document).attr("title", `TBD | ${aID.toUpperCase()}`);
-  //   let contentID = aID + "Content";
-  //   changeContent(contentID);
-  // });
-
   $(window).on("hashchange", changeRoute);
   changeRoute();
 }
 $(document).ready(function () {
   initListeners();
-  setHome();
 });
